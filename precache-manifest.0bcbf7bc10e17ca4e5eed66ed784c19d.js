ff3ad95514ecb9e65e25ecb92c3612135534990b
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-09-23 15:23:31 +0300

    Updates

---
+++ precache-manifest.0bcbf7bc10e17ca4e5eed66ed784c19d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ee77ab62ffda4f65e3e726343eeb05",
+    "url": "/homework-18/index.html"
+  },
+  {
+    "revision": "630a8406f46ddaa8eda0",
+    "url": "/homework-18/static/css/main.dfff2d95.chunk.css"
+  },
+  {
+    "revision": "9d7a8247b962cd9a091e",
+    "url": "/homework-18/static/js/2.9c3dc441.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/homework-18/static/js/2.9c3dc441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630a8406f46ddaa8eda0",
+    "url": "/homework-18/static/js/main.8294920f.chunk.js"
+  },
+  {
+    "revision": "9d6e6afb622bd641c4f4",
+    "url": "/homework-18/static/js/runtime-main.56705767.js"
+  },
+  {
+    "revision": "d964e0b3c644193e5591758f01e91f68",
+    "url": "/homework-18/static/media/padlock.d964e0b3.svg"
+  }
+]);
