9c2987b7a9f14f7e7857f4d0e7407af70c76c686
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-09-23 16:08:50 +0300

    update router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, {Component} from "react";
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import SignIn from "./components/signIn";
 import SignUp from "./components/signUp";
 
-function App() {
-  return (
-    <>
-      <Router>
-        <Switch basename="/homework-18">
-          <Route exact path="/login" render={() => <Redirect to="/login" />}/>
-          <Route path="/login" component={SignIn} />
-          <Route path="/register" component={SignUp} />
-        </Switch>
-      </Router>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Router>
+          <Switch basename="/homework-18">
+            <Route exact path="/" render={() => <Redirect to="/login" />} />
+            <Route path="/login" component={SignIn} />
+            <Route path="/register" component={SignUp} />
+          </Switch>
+        </Router>
+      </>
+    );
+  }
 }
 
 export default App;
